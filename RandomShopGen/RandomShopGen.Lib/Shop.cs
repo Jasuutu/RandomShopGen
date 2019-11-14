eb3f9d7d5a29a41ce5a2d52afa7ba4914526108e
Johnathan Schultz <jschultz@bstglobal.com>
2019-11-14 16:01:18 -0500

    Added the start of shop logic.

--- RandomShopGen/RandomShopGen.Lib/Shop.cs
+++ RandomShopGen/RandomShopGen.Lib/Shop.cs
@@ -1,7 +1,35 @@
-﻿namespace RandomShopGen.Lib
+﻿using System.Collections.Generic;
+using RandomShopGen.Lib.Interfaces;
+
+namespace RandomShopGen.Lib
 {
-    public class Shop
+    public class Shop : IShop
     {
+        private List<Item> itemList;
+        private string name;
+        private int gold;
         
+        public Shop(string name, int gold)
+        {
+            itemList = new List<Item>();
+            this.name = name;
+            this.gold = gold;
+        }
+
+        public string Name => name;
+
+        public int Gold => gold;
+        
+        // take formatted data file and create items for itemList
+        public bool CreateItemList(string fileLocation)
+        {
+            return false;
+        }
+
+        // create output file for the shop and save to the disk
+        public bool CreateShopFile(string fileLocation)
+        {
+            return false;
+        }
     }
 }
--- RandomShopGen/RandomShopGen.Tests/LibTests.cs
+++ RandomShopGen/RandomShopGen.Tests/LibTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using RandomShopGen.Lib;
 
 namespace RandomShopGen.Tests
@@ -8,14 +10,25 @@ namespace RandomShopGen.Tests
     [TestClass]
     public class LibTests
     {
+        private List<Item> items = new List<Item>
+        {
+            new Item("Test1", 1),
+            new Item("Test2", 2),
+            new Item("Test10", 10),
+            new Item("Test Space", 100),
+            new Item("Test3", 3),
+            new Item("Test4", 4)
+        };
+
         [TestMethod]
+        [TestCategory("Item")]
         public void ItemWillNotThrowExceptionWhenGivenProperValues()
         {
             // Setup
             var itemName = "Test Item";
             var itemValue = 100;
-
             Item item = null;
+
             // Execute
             Action act = () =>
             {
@@ -25,6 +38,46 @@ namespace RandomShopGen.Tests
             // Assert
             act.Should().NotThrow<ArgumentException>("a valid name and valid value were provided.");
             item.Should().NotBeNull("item should have been constructed correctly.");
+        }
+
+        [TestMethod]
+        [TestCategory("Item")]
+        public void ItemWillThrowExceptionWhenGivenIncorrectValues()
+        {
+            // Setup
+            var itemName = string.Empty;
+            var itemValue = 100;
+            Item item = null;
+
+            // Execute
+            Action act = () => { item = new Item(itemName, itemValue); };
+
+            // Assert
+            act.Should().Throw<ArgumentException>("an empty item name is not a valid value");
+            item.Should().BeNull("no item should be created when an invalid value is used.");
+        }
+
+        [TestMethod]
+        [TestCategory("Shop")]
+        public void ShopShouldHaveNameWhenValidValuesAreProvided()
+        {
+            // Setup
+            var shopName = "Test Shop";
+            var shopGold = 1000;
+            
+            // Execute
+            var shop = new Shop(shopName, shopGold);
+
+            // Assert
+            shop.Name.Should().Be("Test Shop");
+            shop.Gold.Should().Be(1000);
+        }
+
+        [TestMethod]
+        [TestCategory("Shop")]
+        public void ShouldShouldReturnSuccessWhenCreatingItemList()
+        {
+            
         }
     }
 }
